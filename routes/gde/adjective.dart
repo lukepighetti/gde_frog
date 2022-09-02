658fd1b215f0f583087f4ca90eb59ab5d12756dc
Luke Pighetti <lukepighetti@gmail.com>
2022-09-02 15:48:09 -0400

    add routes for just adverbs and adjectives

---
+++ routes/gde/adjective.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+import 'package:dart_frog/dart_frog.dart';
+import 'package:frog_test/lib.dart';
+
+/// Generate a string with format like Generally Decent Engineer
+/// ie: adverb starts with G + adjective starts with D + 'Engineer'
+Response onRequest(RequestContext context) {
+  final adjective = adjectives[Random().nextInt(adjectives.length - 1)];
+
+  return Response(
+    body: adjective.capitalize(),
+  );
+}
---
+++ routes/gde/adverb.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+import 'package:dart_frog/dart_frog.dart';
+import 'package:frog_test/lib.dart';
+
+/// Generate a string with format like Generally Decent Engineer
+/// ie: adverb starts with G + adjective starts with D + 'Engineer'
+Response onRequest(RequestContext context) {
+  final adverb = adverbs[Random().nextInt(adverbs.length - 1)];
+
+  return Response(
+    body: adverb.capitalize(),
+  );
+}

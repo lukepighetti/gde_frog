8013d936570d62593c0223673d450c6444f5f6a9
Luke Pighetti <lukepighetti@gmail.com>
2022-09-02 16:27:41 -0400

    allow requesting multiple, limited 1-1000

---
+++ routes/gde/[count].dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+import 'package:dart_frog/dart_frog.dart';
+import 'package:gde_frog/lib.dart';
+
+/// Generate a string with format like Generally Decent Engineer
+/// ie: adverb starts with G + adjective starts with D + 'Engineer'
+Response onRequest(RequestContext context, String count) {
+  return Response(
+    body: List.generate(
+      (int.tryParse(count) ?? 1).clamp(1, 1000),
+      (_) {
+        final adverb = adverbs[Random().nextInt(adverbs.length - 1)];
+        final adjective = adjectives[Random().nextInt(adjectives.length - 1)];
+        return '${adverb.capitalize()} ${adjective.capitalize()} Engineer';
+      },
+    ).join('\n'),
+  );
+}
